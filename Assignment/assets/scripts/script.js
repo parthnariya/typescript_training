03f0c25e776edc42a6d95f6fdd2647f64b2da8cc
parth <parth@mail.com>
2022-01-18 12:06:34 +0530

    compiled file

--- Assignment/assets/scripts/script.js
+++ Assignment/assets/scripts/script.js
@@ -2,19 +2,18 @@
 let operationDiv = document.getElementById("operation-div");
 let resultDiv = document.getElementById("result-div");
 /*
-formatNumbersWithComma(input)
-
-this function will display out put with comma standard
-e.g 5000 -> 5,000
-for better readabilty
+convert given string into comma seperated value
+formateNumberWithComm(inputString)
+@param {string} inputString :- which is in normal form
+@return {string} outputString :- comma seperated value of given string usign regex
 */
 function formatNumbersWithComma(input) {
     return input.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
-/* addChar(element : html-element)
-   it add char or input (value) by click on the button
-   generallly it create input string or expression for the output
-
+/*
+generate maths eqution to eval.
+addChar(element : html-element)
+@param {html-element} element :- it is a value assoiated with button
 */
 function addChar(element) {
     let char = element.getAttribute("data-value");
@@ -22,38 +21,37 @@ function addChar(element) {
     currentValue += char;
     operationDiv.innerText = formatNumbersWithComma(currentValue);
 }
-/* plusorMinus()
- this function convert sign of the givan input
- if it is +ve then convert into -ve and vise-versa
+/*
+it change sign of current input
+plusorMinus()
 */
 function plusorMinus() {
     let originalVal = Number(operationDiv.innerText.replace(/,/g, ''));
     originalVal = originalVal * -1;
     operationDiv.innerText = formatNumbersWithComma(originalVal.toString());
 }
-// clearScreen()
-// it clear the result area or we can say display part
+/* clearScreen()
+ it clear the result area or we can say display part */
 function clearScreen() {
     operationDiv.innerText = "";
 }
-// backspace()
-// it clears the last character from the input string 
+/* backspace()
+it clears the last character from the input string */
 function backspace() {
     let operationValue = operationDiv.innerText.replace(/,/g, '');
     let operationValueLength = operationValue.length;
     let newOperationValue = operationValue.substring(0, operationValueLength - 1);
     operationDiv.innerText = formatNumbersWithComma(newOperationValue);
 }
-//Calculate fun.
-// calculate()
-// it will calculate the simple math equation and display
+/* calculate()
+it will calculate the simple math equation and display */
 function calculate() {
     let operationValue = operationDiv.innerText.replace(/,/g, '');
     operationDiv.innerText = formatNumbersWithComma(eval(operationValue).toString());
 }
-// trigonometry function to calculate sine,cos,tan,cot,cosec,sec
-// trigonometry(function name)
-// it calculate trigonometry function value from given input and display
+/* trigonometry function to calculate sine,cos,tan,cot,cosec,sec
+trigonometry(function name)
+@param {string} name :- name of the function which we need  to execute */
 function trigonometry(fun) {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     let result = 0;
@@ -84,8 +82,10 @@ function trigonometry(fun) {
         operationDiv.innerText = formatNumbersWithComma(result.toString());
     }
 }
-// mathFunction(function_name : string)
-// this function will calculate the basic maths function values
+/* calculate the specified mathsfunction's value
+mathFunction(function_name : string)
+@param {string} : function_name -> name of the function which we need to execute
+ */
 function mathFunction(functionName) {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     let result = 0;
@@ -102,47 +102,47 @@ function mathFunction(functionName) {
     }
     operationDiv.innerText = formatNumbersWithComma(result.toString());
 }
-// calc_square()
-// this function calculate the square the given input value
+/* calculate the square the given input value
+calc_square() */
 function calc_square() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationValue *= operationValue;
     operationDiv.innerText = formatNumbersWithComma(operationValue.toString());
 }
-// calc_absolute()
-// this function calculate the absolute value of given input
+/* calculate the absolute value of given input
+calc_absolute() */
 function calc_absolute() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationDiv.innerText = String(Math.abs(operationValue));
 }
-// calc_exp()
-// this function will calculate the exponent value of the given value
+/* calculate the exponent value of the given value
+calc_exp() */
 function calc_exp() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationDiv.innerText = String(Math.exp(operationValue));
 }
-// fixed_exp()
-// this function will fixed value with exponent values
-// e.g 0.1234454e+20 -> 0.12e+20
+/* fixed value with exponent values
+fixed_exp() */
 function fixed_exp() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationDiv.innerText = formatNumbersWithComma(operationValue.toExponential(3));
 }
-// radianToDegree()
-// this function will convert 
+/* convert given radiad value into degree
+radianToDegree()
+ */
 function radianToDegree() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationValue = operationValue * (180 / Math.PI);
     operationDiv.innerText = formatNumbersWithComma(operationValue.toString());
 }
-// calc_sqrt()
-// this function will calculate the square root of given input
+/* calculate the square root of given input
+calc_sqrt() */
 function calc_sqrt() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationDiv.innerText = Math.sqrt(operationValue).toString();
 }
-// calc_factorial()
-// this function will calculate the factorial value of the given value
+/* calculate the factorial value of the given value
+calc_factorial() */
 function calc_factorial() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     let result = 1;
@@ -152,22 +152,20 @@ function calc_factorial() {
     }
     operationDiv.innerText = result.toString();
 }
-// cal_ten_pow()
-// this function will calculate the value of the power of 10
+/* calculate the value of the power of 10
+cal_ten_pow() */
 function calc_ten_pow() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationDiv.innerText = Math.pow(10, operationValue).toString();
 }
-// log base 10 function
-// calc_log()
-// this function will calculate the logerithmic value base 10 and display result
+/* calculate the logerithmic value base 10 and display result
+calc_log() */
 function calc_log() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationDiv.innerText = Math.log10(operationValue).toString();
 }
-//natural logarithm function
-// calc_ln()
-// this function will calculate the logerithmic value base e and display result
+/* calculate the logerithmic value base e and display result
+calc_ln() */
 function calc_ln() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     operationDiv.innerText = Math.log(operationValue).toString();
@@ -176,8 +174,8 @@ function calc_ln() {
 // MC, MR, M+, M-, MS funtionality
 let memoryValue = 0; //this is a memory  which store the memory value
 let mStatus = 0; //this flag is used whether memory is present or not
-// memoryClear()
-// this function will clear or remove the memory values
+/* clear or remove the memory values
+memoryClear() */
 function memoryClear() {
     memoryValue = 0;
     mStatus = 0;
@@ -186,14 +184,13 @@ function memoryClear() {
         document.getElementById("memory-recall").style.opacity = "0.2";
     }
 }
-// memoryRecall()
-// it will calculate the value of memory values and display
+/* calculate the value of memory values and display
+memoryRecall() */
 function memoryRecall() {
     operationDiv.innerText = formatNumbersWithComma(memoryValue.toString());
 }
-// memoryPlus()
-// it will add the input value to the memory stack
-// e.g currentValue of memory stack plus given value
+/* add the input value to the memory stack
+memoryPlus() */
 function memoryPlus() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     if (operationDiv.innerText != "") {
@@ -206,9 +203,8 @@ function memoryPlus() {
     }
     operationDiv.innerText = "";
 }
-// memoryMinus()
-// this function will reduse memory value by the givan value 
-// e.g currentValue of memory stack minus given value
+/* reduse memory value by the givan value
+memoryMinus() */
 function memoryMinus() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     if (operationDiv.innerText != "") {
@@ -222,9 +218,8 @@ function memoryMinus() {
     }
     operationDiv.innerText = "";
 }
-// memorySave()
-// it will put the input value into the memory stack
-// curentValue of memorystack will replace by inputvalue
+/* put the input value into the memory stack
+memorySave() */
 function memorySave() {
     let operationValue = Number(operationDiv.innerText.replace(/,/g, ''));
     if (operationDiv.innerText != "") {
